ce5cb4bb275287fda80757ca9136976aca79f90a
Albertdai-Python <84005182+Albertdai-Python@users.noreply.github.com>
2022-11-09 20:47:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,32 @@ from tkinter import *
 
 
 class Calendar(Canvas):
+
+    def load_txt(self):
+        with open("data_info.dt", 'r') as f:
+            data = [i.split('[-]') for i in f.read().split('\n')]
+            dictionary = {}
+            for i in data:
+                dictionary[i[0]] = [j.split('[@]') for j in i[1].split('[*]')]
+            for i in dictionary:
+                for j in dictionary[i]:
+                    j[1] = j[1].split('~')
+                    j[1][0] = j[1][0].split(":")
+                    j[1][1] = j[1][1].split(":")
+            f.close()
+        return dictionary
+
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         self.month_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        self.params = {"cal_x": 700, "cal_y": 600, "border": 10, "year": 2022, "month": 11, "day": 2, "side": 300, "primary_color": 'red', "secondary_color": 'purple', "dark": False}
-        self.objs = {"obj_list": [], "txt_list": []}
+        self.params = {"cal_x": 1000, "cal_y": 800, "border": 20, "year": 2022, "month": 11, "day": 2, "side": 500, "primary_color": '#dee72d', "secondary_color": '#bebdac', "dark": False}
+        self.objs = {"obj_list": [], "txt_list": [], "side_txt": [], "side_line": [], "side_block": []}
         self.cal_data = []
         self.master = master
         self.ml = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
               'November', 'December']
+        self.place(x=0, y=0)
+        self.notes = self.load_txt()
 
     def set_param(self, **kwargs):
         for i in list(kwargs.keys()):
@@ -37,6 +54,31 @@ class Calendar(Canvas):
             temp_cal[i] = i - start_day + 1
         self.cal_data = [temp_cal[i:i + 7] for i in range(0, len(temp_cal), 7)]
 
+    def display_side(self, event):
+        if event.x <= self.params["cal_x"] + self.params["border"] and event.y <= self.params["cal_x"] + self.params["border"]:
+            item_index = int(self.gettags(self.find_closest(event.x, event.y)[0])[0]) - 1
+            self.params["day"] = item_index + 1
+        date_string = str(self.params["year"])+'/'+str(self.params["month"])+'/'+str(self.params["day"])
+        note_keys = self.notes.keys()
+        if date_string not in note_keys:
+            info = 'blank'
+        else:
+            for i in self.objs["side_block"]:
+                self.delete(i)
+            self.objs["side_block"] = []
+            info = self.notes[date_string]
+            delta_y = self.params["cal_y"] / 1440
+            for i in info:
+                name = i[0]
+                st_time = int(i[1][0][0]) * 60 + int(i[1][0][1])
+                ed_time = int(i[1][1][0]) * 60 + int(i[1][1][1])
+                st_time *= delta_y
+                ed_time *= delta_y
+                self.objs["side_block"].append(self.create_rectangle(self.params["cal_x"]+2*self.params["border"]+17, st_time, self.params["cal_x"]+2*self.params["border"]+self.params["side"], ed_time, width=0, fill=self.params["primary_color"]))
+                self.objs["side_block"].append(self.create_text(self.params["cal_x"]+2*self.params["border"]+8.5+self.params["side"]/2, (st_time+ed_time)/2, text=name))
+
+        print(info)
+
     def draw_cal(self):
         self.master.geometry(f'{self.params["border"]*3+self.params["cal_x"]+self.params["side"]}x{self.params["border"]*2+self.params["cal_y"]}')
         self.config(width=self.params["border"]*3+self.params["cal_x"]+self.params["side"], height=self.params["border"]*2+self.params["cal_y"])
@@ -56,17 +98,21 @@ class Calendar(Canvas):
                 if self.cal_data[i][j] != 0:
                     self.objs["obj_list"].append(
                         self.create_rectangle(self.params["border"] + j * self.params["cal_x"]/7, self.params["border"] + i * self.params["cal_y"]/6, self.params["border"] + (j+1) * self.params["cal_x"]/7, self.params["border"] + (i+1) * self.params["cal_y"]/6, fill=self.params["primary_color"],
-                                           activefill=self.params["secondary_color"], width=3, outline=color2))
-                    self.objs["txt_list"].append(self.create_text(self.params["border"] + (j+0.5) * self.params["cal_x"]/7, 2*self.params["border"] + i * self.params["cal_y"]/6, text=str(self.cal_data[i][j])))
+                                           activefill=self.params["secondary_color"], width=3, outline=color2, tags=f'{self.cal_data[i][j]}'))
+                    self.bind('<Button-1>', self.display_side)
+                    self.objs["txt_list"].append(self.create_text(self.params["border"] + (j+0.5) * self.params["cal_x"]/7, (i+0.25) * self.params["cal_y"]/6, text=str(self.cal_data[i][j]), tags=f'{self.cal_data[i][j]}', font='Arial 24'))
                 else:
                     self.objs["txt_list"].append(0)
                     self.objs["obj_list"].append(
-                        self.create_rectangle(self.params["border"] + j * self.params["cal_x"]/7, self.params["border"] + i * self.params["cal_y"]/6, self.params["border"] + (j+1) * self.params["cal_x"]/7, self.params["border"] + (i+1) * self.params["cal_y"]/6, fill=color, width=3, outline=color2))
+                        self.create_rectangle(self.params["border"] + j * self.params["cal_x"]/7, self.params["border"] + i * self.params["cal_y"]/6, self.params["border"] + (j+1) * self.params["cal_x"]/7, self.params["border"] + (i+1) * self.params["cal_y"]/6, fill=color, width=3, outline=color2, tags='0'))
+        delta_y = self.params["cal_y"] / 24
+        for i in range(25):
+            self.objs["side_txt"].append(self.create_text(self.params["cal_x"]+2*self.params["border"]+7.5, self.params["border"]+delta_y*i, text=str(i), fill=color2))
+            self.objs["side_line"].append(self.create_line(self.params["cal_x"]+2*self.params["border"]+17, self.params["border"]+delta_y*i, self.params["cal_x"]+2*self.params["border"]+self.params["side"], self.params["border"]+delta_y*i, fill=color2, width=2))
 
 
 w = Tk()
 w.geometry('720x620')
 c = Calendar(w)
 c.draw_cal()
-c.pack()
 w.mainloop()
